5f445f4ed425100cc90e402132b9e609aca67bfb
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2022-11-08 00:08:47 +0600

    connect with database and create .env file and user and password hide

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -7,6 +10,27 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uhbaknf.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const dbConnect = async () => {
+  try {
+    await client.connect();
+  } catch (error) {
+    console.error(error);
+  }
+};
+dbConnect();
+
+const serviceCollection = client.db("photography").collection("services");
 
 // root api
 app.get("/", (req, res) => {

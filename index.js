9dcb64d891e684c43a01610e14483b476c367b83
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2022-11-08 17:32:12 +0600

    dynamically find service with id api create

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -12,8 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uhbaknf.mongodb.net/?retryWrites=true&w=majority`;
-
-console.log(uri);
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -31,6 +29,63 @@ const dbConnect = async () => {
 dbConnect();
 
 const serviceCollection = client.db("photography").collection("services");
+
+app.get("/service", async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit);
+    const query = {};
+    const cursor = serviceCollection.find(query);
+    const service = await cursor.limit(limit).toArray();
+    res.send({
+      status: true,
+      message: "Data find successfully",
+      data: service,
+    });
+  } catch (error) {
+    console.error(error);
+    res.send({
+      status: false,
+      message: error,
+    });
+  }
+});
+
+app.get("/services", async (req, res) => {
+  try {
+    const query = {};
+    const cursor = serviceCollection.find(query);
+    const services = await cursor.toArray();
+    res.send({
+      status: true,
+      message: "Data find successfully",
+      data: services,
+    });
+  } catch (error) {
+    console.error(error);
+    res.send({
+      status: false,
+      message: error,
+    });
+  }
+});
+app.get("/services/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const services = await serviceCollection.findOne(query);
+    res.send({
+      status: true,
+      message: "Data find successfully",
+      data: services,
+    });
+  } catch (error) {
+    console.error(error);
+    res.send({
+      status: false,
+      message: error,
+    });
+  }
+});
 
 // root api
 app.get("/", (req, res) => {
